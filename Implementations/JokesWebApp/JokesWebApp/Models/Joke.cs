227078cdf65958d4a71ad717e89a22ac77f1c3bb
Shtaiwee <shtaiweeomair@gmail.com>
2022-06-01 23:21:50 +0300

    implement

---
+++ Implementations/JokesWebApp/JokesWebApp/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+
+        public String JokeQuestion { get; set; }
+
+        public String JokeAnswer { get; set; }
+
+
+        public Joke()
+        {
+                
+        }
+    }
+}
